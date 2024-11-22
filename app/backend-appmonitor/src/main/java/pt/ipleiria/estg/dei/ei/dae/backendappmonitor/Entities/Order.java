8dd5b41558202e0a3b3af2174596fd6dafbaea93
JosePereira114 <2211033@my.ipleiria.pt>
2024-11-22 17:00:16 +0000

    Entitites

---
+++ app/backend-appmonitor/src/main/java/pt/ipleiria/estg/dei/ei/dae/backendappmonitor/Entities/Order.java
@@ -0,0 +1,68 @@
+package pt.ipleiria.estg.dei.ei.dae.backendappmonitor.Entities;
+
+import javax.persistence.Entity;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+@Entity
+public class Order extends Versionable {
+    private long id;
+    private Date createdDate;
+    private Date deliveredDate;
+    private Customer customer;
+    private List<Volume> volumes;
+
+
+    public Order() {
+    }
+
+    public Order(long id, Date createdDate, Date deliveredDate, Customer customer, List<Volume> volumes) {
+        this.id = id;
+        this.createdDate = createdDate;
+        this.deliveredDate = deliveredDate;
+        this.customer = customer;
+        this.volumes = volumes;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public Date getCreationDate() {
+        return createdDate;
+    }
+
+    public void setCreationDate(Date creationDate) {
+        this.createdDate = creationDate;
+    }
+
+    public Date getDeliveredDate() {
+        return deliveredDate;
+    }
+
+    public void setDeliveredDate(Date deliveredDate) {
+        this.deliveredDate = deliveredDate;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
+
+    public List<Volume> getVolumes() {
+        return new ArrayList<>(volumes);
+    }
+
+    public void setVolumes(List<Volume> volumes) {
+        this.volumes = volumes;
+    }
+}
---
+++ app/backend-appmonitor/src/main/java/pt/ipleiria/estg/dei/ei/dae/backendappmonitor/Entities/PackageType.java
@@ -0,0 +1,55 @@
+package pt.ipleiria.estg.dei.ei.dae.backendappmonitor.Entities;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PackageType extends Versionable{
+    private long id;
+    private String name;
+    private List<SensorType> mandatorySensors;
+    private Volume volume;
+
+
+    public PackageType() {
+    }
+
+    public PackageType(long id, String name, List<SensorType> mandatorySensors, Volume volume) {
+        this.id = id;
+        this.name = name;
+        this.mandatorySensors = mandatorySensors;
+        this.volume = volume;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<SensorType> getMandatorySensors() {
+        return new ArrayList<>(mandatorySensors);
+
+    }
+
+    public void setMandatorySensors(List<SensorType> mandatorySensors) {
+        this.mandatorySensors = mandatorySensors;
+    }
+
+    public Volume getVolume() {
+        return volume;
+    }
+
+    public void setVolume(Volume volume) {
+        this.volume = volume;
+    }
+}
