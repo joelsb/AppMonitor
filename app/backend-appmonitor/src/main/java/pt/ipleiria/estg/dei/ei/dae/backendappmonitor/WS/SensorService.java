1a58ac1645e289c10a97ed04ecee835e17a9c723
JosePereira114 <2211033@my.ipleiria.pt>
2024-12-13 16:10:41 +0000

    SensorService

---
+++ app/backend-appmonitor/src/main/java/pt/ipleiria/estg/dei/ei/dae/backendappmonitor/WS/SensorService.java
@@ -0,0 +1,26 @@
+package pt.ipleiria.estg.dei.ei.dae.backendappmonitor.WS;
+
+import jakarta.ejb.EJB;
+import jakarta.ws.rs.*;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+import pt.ipleiria.estg.dei.ei.dae.backendappmonitor.DTOs.SensorDTO;
+import pt.ipleiria.estg.dei.ei.dae.backendappmonitor.EJBs.SensorBean;
+import pt.ipleiria.estg.dei.ei.dae.backendappmonitor.Entities.Sensor;
+import pt.ipleiria.estg.dei.ei.dae.backendappmonitor.Exceptions.MyEntityNotFoundException;
+
+@Path("sensors")
+@Produces({MediaType.APPLICATION_JSON})
+@Consumes({MediaType.APPLICATION_JSON})
+public class SensorService {
+    @EJB
+    private SensorBean sensorBean;
+
+    @GET
+    @Path("/{id}")
+    public Response getSensorData(@PathParam("id") long id) throws MyEntityNotFoundException {
+        var history = sensorBean.find(id).getHistory();
+        var sensorDTO = SensorDTO.from(sensorBean.find(id));
+        return Response.ok().build();
+    }
+}
